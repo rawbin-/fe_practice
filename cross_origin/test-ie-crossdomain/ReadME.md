f9d9d185b8830a2fbb29e94d15a9e43540d8739e
张钌 <zhangliao@baiwang.com>
2017-11-11 10:12:45 +0800

    增加跨域header的demo

---
+++ cross_origin/test-ie-crossdomain/ReadME.md
@@ -0,0 +1,4 @@
+### 操作步骤
+- 需要node环境，使用 `node server.js` 启动服务端，并可以在启动界面查看请求header
+- 配置host `test.com test.org`到启动上面server的ip上
+- 打开index.html 查看效果
--- cross_origin/test-ie-crossdomain/server.js
+++ cross_origin/test-ie-crossdomain/server.js
@@ -4,12 +4,13 @@ var server = new http.Server();
 server.listen(9000);
 
 server.on("request",function(request,response){
-    //不加这个相应头，客户端会报错，无法跨越发请求
+    console.log(request.headers)
+    console.log('------------------')
+    //不加这个响应头，客户端会报错，无法跨越发请求
     response.setHeader("Access-Control-Allow-Origin","*")
 	response.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS")
-	//response.setHeader("Access-Control-Allow-Headers","customHeader,testHeader")
+	response.setHeader("Access-Control-Allow-Headers","customHeader,testHeader,Authorization,Content-Type,X-Requested-With")
     response.writeHead(200,{"Content-Type":"application/json;charset=UTF-8"});
-//    response.write('msg: "cross origin by cors"');
 	response.write(JSON.stringify({
 		success:true,
 		data:{result:1}
